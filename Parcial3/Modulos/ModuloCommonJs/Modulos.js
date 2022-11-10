957c65301fb7dd1d9063f6f9b341ca2bb56c988e
RodriguezCa <alexrdzcar00@gmail.com>
2022-11-09 21:58:34 -0600

    Nuevos cambios

---
+++ Parcial3/Modulos/ModuloCommonJs/Modulos.js
@@ -0,0 +1,9 @@
+const cuadrado = (valor) => { return valor * valor};
+
+exports.cuadrado = cuadrado;
+
+const PI = Math.PI;
+
+exports.area = (r) => PI * r * r;
+exports.circumference = (r) => 2 * PI *r;
+
---
+++ Parcial3/Modulos/ModuloCommonJs/app.js
@@ -0,0 +1,9 @@
+const Modulos = require ("./Modulos.js");
+
+console.log(Modulos.cuadrado(2));
+
+
+
+const Modulos = require('./Modulos.js');
+
+console.log('El area de 1 circulo de radio 4 es: '+Modulos.area(4));
