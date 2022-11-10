f4d017973120517fe8344af75f4c431e28041f9d
RodriguezCa <alexrdzcar00@gmail.com>
2022-11-09 19:43:26 -0600

    Nuevos cambios

---
+++ Parcial3/Modulos/Modulo Nativo/Modulo.js
@@ -0,0 +1,17 @@
+//Sistema de Modulos         Implementacion            Keyboard
+//*CommonJs                  *Node                    *Exports/requires
+//*AMD                       *Requires.js             *Define
+//Modulos Natvios            *ECMAScript              *Export/import
+//Que es un Modulo? Archivo de js que exporta funcionalidad
+export function Mayusculas(cadena)
+{
+return cadena.toUpperCase();
+}
+export function Longitud(cadena)
+{
+    return cadena.length;
+}
+export function Cubo(x)
+{
+    return x*x*x;
+}
