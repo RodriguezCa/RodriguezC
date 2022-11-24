d8e0a968fba54a355b8cb30e2df659bd04e3a3ca
RodriguezCa <alexrdzcar00@gmail.com>
2022-11-23 20:18:30 -0600

    Nuevo cambios

---
+++ Parcial3/Consulta php-mysql/index.php
@@ -0,0 +1,13 @@
+<?php
+    $server= "localhost";
+    $db= "datosempleado";
+    $user= "root";
+    $pass= "";
+    $con = mysqli_connect($server,$user,$pass,$db) or die("No se pudo conectar a la base de datos");
+    $query= "select * from empleado";
+    $registros= mysqli_query($con,$query) or die("Problemas en la consulta");
+
+    $result= mysqli_fetch_all($registros);
+    mysqli_close($con);
+    echo json_encode($result)
+?>
