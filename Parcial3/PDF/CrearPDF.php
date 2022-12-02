ff8ab99fb71456bf4669145e847bbac941e89ab5
RodriguezCa <alexrdzcar00@gmail.com>
2022-12-02 17:16:14 -0600

    Nuevo cambios

---
+++ Parcial3/PDF/CrearPDF.php
@@ -0,0 +1,17 @@
+<?php
+
+require("FPDF./fpdf.php");
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'ALEX EDUARDO RODRIGUEZ CARDONA');
+$pdf->Output();
+
+?>
+
+
+
+
+
+
