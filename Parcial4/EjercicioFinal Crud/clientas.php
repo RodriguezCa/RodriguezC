21b305a3c332c9f81160561568b9e7078da0f8d9
RodriguezCa <alexrdzcar00@gmail.com>
2022-12-07 17:52:34 -0600

    Nuevos cambios

--- Parcial4/EjercicioFinal Crud/clientas.php
+++ Parcial4/EjercicioFinal Crud/clientas.php
@@ -16,7 +16,7 @@ $row = mysqli_fetch_array($query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Elsa Rodriguez</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
@@ -24,25 +24,26 @@ $row = mysqli_fetch_array($query);
 <body>
     <div class="container mt-5">
         <div class="row">
+            <h1 class="text-center ">ELSA RODRIGUEZ</h1>
 
-            <div class="col-sm-2">
-                <h1>NUEVAS CLIENTAS </h1>
-                <form action="insertar.php" method="POST">
+            <div class="col-lg-12">
+                <h2>AGREGAR CLIENTAS</h1>
+                    <form action="insertar.php" method="POST">
 
-                    <input type="text" class="form-control mb-2" name="Nombre" placeholder="Nombre">
-                    <input type="text" class="form-control mb-2" name="Producto" placeholder="Producto">
-                    <input type="text" class="form-control mb-2" name="Precio" placeholder="Precio">
-                    <input type="text" class="form-control mb-2" name="Direccion" placeholder="Direccion">
-                    <input type="text" class="form-control mb-2" name="Lugar" placeholder="Lugar">
+                        <input type="text" class="form-control mb-2" name="Nombre" placeholder="Nombre">
+                        <input type="text" class="form-control mb-2" name="Producto" placeholder="Producto">
+                        <input type="text" class="form-control mb-2" name="Precio" placeholder="Precio">
+                        <input type="text" class="form-control mb-2" name="Direccion" placeholder="Direccion">
+                        <input type="text" class="form-control mb-2" name="Lugar" placeholder="Lugar">
 
-                    <input type="submit" class="btn btn-success">
-                </form>
+                        <input type="submit" class="btn btn-success">
+                    </form>
             </div>
 
-            <div class="col-lg-10">
-                <table class="table">
-                    <thead class="table-sucess table-stripped table-danger">
-                        <tr class="row-succes row-stripped row-primary">
+            <div class="col-lg-12">
+                <table class="table mt-5 table-primary">
+                    <thead class="table-sucess table-stripped table-dark">
+                        <tr>
                             <th>Nombre</th>
                             <th>Producto</th>
                             <th>Precio</th>
